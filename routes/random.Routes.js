8048d3da863f75fc30c274c4d3d458a146b28897
unknown <you@example.com>
2021-12-25 12:44:40 -0300

    se arregla obj

--- routes/random.Routes.js
+++ routes/random.Routes.js
@@ -5,13 +5,31 @@ router.get('/', (req, res) => {
     
     const numRandom = req.query.cant
     let random = []
+    let unicosElementos = []
+    let vecesRepetidos = []
+    let contador = 1
+    let obj = []
 
     for (let i = 0; i < 100; i++) {
         let result = Math.floor(Math.random() * numRandom)
         random.push(result)
     }
 
-    res.send(random)
+    for(let i = 0; i< random.sort().length; i++){
+        if(random[i+1] === random[i]){
+           contador++
+        }else{
+            unicosElementos.push(random[i])
+            vecesRepetidos.push(contador)
+            contador = 1
+        }
+    }
+
+    for(let j = 0; j < unicosElementos.length; j++){
+          obj.push(`${unicosElementos[j]}: se repite ${vecesRepetidos[j]} `) 
+    }
+
+    res.json({obj})
 })
 
 
